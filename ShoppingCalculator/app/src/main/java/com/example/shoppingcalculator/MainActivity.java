5920a8fd6519bc94d127bf6134a4ee9be0a6e91d
CosmicSteve <47962483+CosmicSteve@users.noreply.github.com>
2019-02-25 02:04:34 -0600

    Pushing minor changes

--- ShoppingCalculator/app/src/main/java/com/example/shoppingcalculator/MainActivity.java
+++ ShoppingCalculator/app/src/main/java/com/example/shoppingcalculator/MainActivity.java
@@ -21,12 +21,22 @@ public class MainActivity extends AppCompatActivity {
     public static double Total = 0.0;
     public double sales_Tax = 0.0;
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        computeTotal();
+        total.setText(Double.toString(Total));
+    }
+
+
     public void computeTotal() {
+        Total = 0.0;
         for (Item item: shoppingItems){
             Total += item.getTotal();
             double temp = Total * sales_Tax;
             Total = Total + temp;
         }
+        Total = Math.round(Total*100.0) / 100.0;  //Rounding to 2 decimal places
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,14 +56,14 @@ public class MainActivity extends AppCompatActivity {
         salesTax = findViewById(R.id.sales_tax);
 
         // SALES TAX (8.25% by default)
-        sales_Tax = Double.parseDouble(salesTax.getText().toString()) / 100;
 
         compute.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                sales_Tax = Double.parseDouble(salesTax.getText().toString()) / 100;
                 String name = itemName.getText().toString();
                 double price = Double.parseDouble(itemPrice.getText().toString());
+                price = Math.round(price*100.0) / 100.0;  //Rounding to 2 decimal places
                 double quantity = Double.parseDouble(itemQuantity.getText().toString());
                 shoppingItems.add(new Item(name, price, quantity));
                 if (quantity > 0 && price > 0) {
@@ -76,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
 
         // Changing to addItem activity upon button press
         addItem.setOnClickListener(new View.OnClickListener() {
